1f3960075877555cb4d5b695c26f644b80fa0af1
Fabrizio Durand <fabrizio.durand9106@gmail.com>
2022-11-16 16:51:25 +0100

    exercice done. Search engine not done, no time for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Flat.destroy_all
+10.times do
+  Flat.create!(
+    name: Faker::Commerce.department,
+    address: Faker::Address.country,
+    description: Faker::Company.catch_phrase,
+    price_per_night: (25..500).to_a.sample,
+    number_of_guests: (1..5).to_a.sample,
+    image: "https://picsum.photos/id/#{rand(1..1000)}/3000"
+  )
+end
